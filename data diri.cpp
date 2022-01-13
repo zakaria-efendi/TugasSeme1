c2cac0c6476be6787aed5b751381f9d86a6a4446
zakaria-efendi <97669185+zakaria-efendi@users.noreply.github.com>
2022-01-13 17:29:31 +0700

    Add files via upload

---
+++ data diri.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+	char nama[25];
+	int nim;
+	char prodi[50];
+	
+	printf("\nMuhammad Zakaria Efendi",nama);
+	printf("\nProdi Teknik Informatika",prodi);
+	printf("\n07.1.2021.0027",nim);
+	
+	return 0;
+}
